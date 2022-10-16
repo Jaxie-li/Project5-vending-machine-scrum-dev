692113e281f378e15333d2606b6f844c2b0e8971
Ye Yuan <yyua9492@uni.sydney.edu.au>
2022-10-16 16:47:46 +1100

    Finished User 14 15 16(cash payment)

--- src/main/java/vendingmachine/controller/ListProductController.java
+++ src/main/java/vendingmachine/controller/ListProductController.java
@@ -38,6 +38,16 @@ public class ListProductController {
         stage.show();
     }
 
+    public void checkOut(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vendingmachine/GUI/CashPayment.fxml"));
+        root = loader.load();
+
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void init(AppController appController){
         this.appController = appController;
 
