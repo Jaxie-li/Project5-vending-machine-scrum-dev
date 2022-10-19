f9b24ce9ec1b3a51ee38ecc1e1244608194a2b8e
Ye Yuan <yyua9492@uni.sydney.edu.au>
2022-10-19 23:24:59 +1100

    Update CashPaymentController.java

--- src/main/java/vendingmachine/controller/CashPaymentController.java
+++ src/main/java/vendingmachine/controller/CashPaymentController.java
@@ -8,7 +8,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+import vendingmachine.utils.DBModel;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -90,7 +92,7 @@ public class CashPaymentController {
 
             Optional<ButtonType> result = alert.showAndWait();
             if (result.isEmpty()) {
-                System.out.println("Error: pop up button!");
+                System.out.println("Error!");
             } else if (result.get() == ButtonType.OK) {
 
                 System.out.println("you Successfully cancel the transaction.");
@@ -108,6 +110,32 @@ public class CashPaymentController {
                 errorFlag.setText("Insert enough balance!");
                 System.out.println("Insert enough balance!");
             }
+
+        } else {
+            // check out successfully
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("");
+            alert.setHeaderText("Welcome to use ! ");
+
+            // TBD
+            String checkOutInfo = "You will get your products " + " " + " and changes " + " ";
+            alert.setContentText(checkOutInfo);
+
+            Optional<ButtonType> button = alert.showAndWait();
+
+            if (button.isEmpty()) {
+                System.out.println("Error");
+            } else if (button.get() == ButtonType.OK) {
+                System.out.println("Click OK");
+                // go back to previous page and log out
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/vendingmachine/GUI/App.fxml"));
+                Parent root = loader.load();
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
         }
     }
 
@@ -116,26 +144,44 @@ public class CashPaymentController {
      * @param event click sum button to sum the balance
      */
     @FXML
-    public void sum(ActionEvent event) {
-        int hundredDollar = Integer.parseInt(hundredDollarsAmount.getText());
-        int fiftyDollar = Integer.parseInt(fiftyDollarsAmount.getText());
-        int twentyDollar = Integer.parseInt(twentyDollarsAmount.getText());
-        int tenDollar = Integer.parseInt(tenDollarsAmount.getText());
-        int fiveDollar = Integer.parseInt(fiveDollarsAmount.getText());
-        int twoDollar = Integer.parseInt(twoDollarsAmount.getText());
-        int oneDollar = Integer.parseInt(oneDollarAmount.getText());
+    public void sum(ActionEvent event) throws IOException {
+        try {
+            int hundredDollar = Integer.parseInt(hundredDollarsAmount.getText());
+            int fiftyDollar = Integer.parseInt(fiftyDollarsAmount.getText());
+            int twentyDollar = Integer.parseInt(twentyDollarsAmount.getText());
+            int tenDollar = Integer.parseInt(tenDollarsAmount.getText());
+            int fiveDollar = Integer.parseInt(fiveDollarsAmount.getText());
+            int twoDollar = Integer.parseInt(twoDollarsAmount.getText());
+            int oneDollar = Integer.parseInt(oneDollarAmount.getText());
 
-        int fiftyCent = Integer.parseInt(fiftyCentsAmount.getText());
-        int twentyCent = Integer.parseInt(twentyCentsAmount.getText());
-        int tenCent = Integer.parseInt(tenCentsAmount.getText());
-        int fiveCent = Integer.parseInt(fiveCentsAmount.getText());
+            int fiftyCent = Integer.parseInt(fiftyCentsAmount.getText());
+            int twentyCent = Integer.parseInt(twentyCentsAmount.getText());
+            int tenCent = Integer.parseInt(tenCentsAmount.getText());
+            int fiveCent = Integer.parseInt(fiveCentsAmount.getText());
 
-        Double amount = 100.00 * hundredDollar + 50.00 * fiftyDollar + 20.00 * twentyDollar + 10.00 * tenDollar
-                + 5.00 * fiveDollar + 2.00 * twoDollar + 1.00 * oneDollar + 0.50 * fiftyCent + 0.20 * twentyCent
-                + 0.10 * tenCent + 0.05 * fiveCent;
+            Double amount = 100.00 * hundredDollar + 50.00 * fiftyDollar + 20.00 * twentyDollar + 10.00 * tenDollar
+                    + 5.00 * fiveDollar + 2.00 * twoDollar + 1.00 * oneDollar + 0.50 * fiftyCent + 0.20 * twentyCent
+                    + 0.10 * tenCent + 0.05 * fiveCent;
 
-        this.balance = amount;
-        inputAmount.setText(String.format("%.2f", amount));
+            this.balance = amount;
+            inputAmount.setText(String.format("%.2f", amount));
+
+//            DBModel db = new DBModel() {
+//                @Override
+//                public JSONObject serialize() {
+//                    return null;
+//                }
+//            };
+
+        } catch (NumberFormatException nfe) {
+
+            System.out.println("Input error");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("");
+            alert.setHeaderText("Insert Error :( ");
+            alert.setContentText("Check input :<");
+
+            alert.showAndWait();
+        }
     }
-
 }
