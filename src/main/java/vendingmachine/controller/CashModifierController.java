4fdfbaa049c983254f4662a3ca6687a947842d68
jgan9883 <80942109+jgan9883@users.noreply.github.com>
2022-10-27 20:56:04 +1100

    Finished cashier reports generation and successful message

--- src/main/java/vendingmachine/controller/CashModifierController.java
+++ src/main/java/vendingmachine/controller/CashModifierController.java
@@ -1,10 +1,12 @@
 package vendingmachine.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +15,39 @@ public class CashModifierController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private TextField fiftyCentsAmount;
+
+    @FXML
+    private TextField fiftyDollarsAmount;
+
+    @FXML
+    private TextField fiveCentsAmount;
+
+    @FXML
+    private TextField fiveDollarsAmount;
+
+    @FXML
+    private TextField hundredDollarsAmount;
+
+    @FXML
+    private TextField oneDollarAmount;
+
+    @FXML
+    private TextField tenCentsAmount;
+
+    @FXML
+    private TextField tenDollarsAmount;
+
+    @FXML
+    private TextField twentyCentsAmount;
+
+    @FXML
+    private TextField twentyDollarsAmount;
+
+    @FXML
+    private TextField twoDollarsAmount;
 
     private AppController appController;
     public void cashierMain(ActionEvent event) throws IOException {
