15a36f78a5ca30f99c978936559e14bd083dfefc
Yue Li <yuli8032@uni.sydney.edu.au>
2022-10-30 21:50:20 +1100

    fix cashier list product

--- src/main/java/vendingmachine/controller/CashierController.java
+++ src/main/java/vendingmachine/controller/CashierController.java
@@ -79,7 +79,9 @@ public class CashierController {
     public void listProduct(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/vendingmachine/GUI/ListProduct.fxml"));
         root = loader.load();
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        ListProductController listProductController = loader.getController();
+        listProductController.init(appController);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
